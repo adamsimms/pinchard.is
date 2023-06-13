e06322d7411816a34bf728e2a4880761f3cdbd64
Adam Simms <hello@adamsim.ms>
2023-06-13 07:17:29 -0400

    Updated Google Analytics Tag

--- resettled/index.php
+++ resettled/index.php
@@ -67,15 +67,13 @@
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB8lvMmq3uGYrxxUPsBIfjeeNGDqKLeqMo&callback=myMap"></script>
 
     <!-- Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-G1XKSQNT5M"></script>
     <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+         window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
 
-      ga('create', 'UA-106248699-1', 'auto');
-      ga('send', 'pageview');
-
+         gtag('config', 'G-G1XKSQNT5M');
     </script>
 
 </body>
